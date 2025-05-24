ca5185765c2ac3f0d9b8555e2680d65f8914b806
Jolan Bertin <jolan.bertin@edu.univ-fcomte.fr>
2025-05-24 18:07:14 +0200

    fix css

--- script.js
+++ script.js
@@ -16,16 +16,18 @@ function deplace() {
     }
     else {
         projet.style.transition = "0.5s"
+        projet.style.marginTop = "180px"
+        projet.style.transform = "none"
+
         spanProjet.style.transition = "transform 0.5s"
         spanProjet.style.transform = "rotate(180deg)"
-        projet.style.marginTop = "180%"
         spanProjet.style.backgroundColor = "var(--color1)"
         spanProjet.style.color = "var(--white)"
     }
 }
 
 function retourne() {
-    if (document.getElementById("projet").style.marginTop == "180%") {
+    if (document.getElementById("projet").style.marginTop == "180px") {
         spanProjet.style.transform = "rotate(360deg)"
         spanProjet.style.backgroundColor = "var(--color2)"
     }
