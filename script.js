7b9f9d063aa0ceb06943d25c60a05f9045bee2ad
Jolan Bertin <jolan.bertin@edu.univ-fcomte.fr>
2025-06-03 18:13:00 +0200

    maj bouton projet et création page projet

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+function accueil() {
+    window.location.href = 'index.html'
+}
+
+// PAGE ACCUEIL
+
 function afficheCV() {
+
+    const cv = document.getElementById("cv");
+    const cvPdf = document.getElementById("cvPdf");
+    const retourCV = document.getElementById("retourCV");
+    const spanRetourCV = document.getElementById("spanRetourCV");
+
     cv.style.height = "100%"
     cv.style.width = "100%"
 
@@ -14,61 +26,54 @@ function afficheCV() {
     spanRetourCV.style.transitionDelay = "1s"
 }
 
-function cacheCV() {
+function cacheCV(e) {
+
+    const cv = document.getElementById("cv");
+    const spanRetourCV = document.getElementById("spanRetourCV");
+
     cv.style.height = "0%"
     cv.style.width = "0%"
 
-    retourCV.style.height = "0%"
-    retourCV.style.width = "0%"
-    retourCV.style.border = "none"
+    e.style.height = "0%"
+    e.style.width = "0%"
+    e.style.border = "none"
 
     spanRetourCV.style.fontSize = "0"
     spanRetourCV.style.transitionDelay = "0s"
 
 }
 
-function deplace() {
+function deplace(e) {
+    if (e.style.marginTop == "32vh") {
 
-    if (document.getElementById("projet").style.marginTop == "32vh") {
+        const spanProjet = document.getElementById("spanProjet");
+
         spanProjet.style.transform = "rotate(360deg)"
         spanProjet.style.backgroundColor = "var(--color2)"
-    }
-    else if (document.getElementById("projet").style.marginTop != "180px") {
-        projet.style.transition = "0.5s"
-        projet.style.marginTop = "32vh"
-        projet.style.transform = "none"
-
-        spanProjet.style.transition = "transform 0.5s"
-        spanProjet.style.transform = "rotate(180deg)"
-        spanProjet.style.backgroundColor = "var(--color1)"
-        spanProjet.style.color = "var(--white)"
+        spanProjet.style.color = "var(--black)"
     }
 }
 
-function changePage() {
+function changePage(e) {
 
-    if (document.getElementById("spanProjet").style.backgroundColor == "var(--color2)") {
+    const spanProjet = document.getElementById("spanProjet");
+
+    if (e.style.marginTop != "32vh") {
+        {
+            e.style.transition = "0.5s"
+            e.style.marginTop = "32vh"
+            e.style.transform = "none"
+
+            spanProjet.style.transition = "transform 0.5s"
+            spanProjet.style.transform = "rotate(180deg)"
+            spanProjet.style.backgroundColor = "var(--color1)"
+            spanProjet.style.color = "var(--white)"
+        }
+    }
+    else if (spanProjet.style.backgroundColor == "var(--color2)") {
         spanProjet.style.backgroundColor = "var(--white)"
-        spanProjet.style.color = "var(--black)"
         window.location.href = "projets.html"
     }
 }
 
-// function entrer(lieu, event) {
-//     const scene = document.getElementById("grandeScene"); //la c'est l'image que tu as de base en fond
-//     const bg = document.getElementById("cv");
-
-//     const x = event.clientX;  //c'est pour faire le zoom sur l'endroit ou tu clique
-//     const y = event.clientY;
-
-//     const xPercent = (x / window.innerWidth) * 100;
-//     const yPercent = (y / window.innerHeight) * 100;
-
-//     scene.style.transformOrigin = { xPercent }; { yPercent };
-
-//     scene.classList.add("zoom");
-//     scene.classList.remove("no-zoom");
-
-//     scene.classList.remove("zoom");
-//     scene.classList.add("no-zoom");
-// }
+// PAGE ACCUEIL
